c5c1561a2f51c4414121e1b4013dce4d6adeb9a2
AvoKoppas <avo.okas@live.com>
2021-05-31 16:15:35 +0300

    Getting data for english lower leagues But it's not pretty

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useCallback, useEffect, useState} from 'react';
+import axios from "axios";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [responseData, setResponseData] = useState(['Here comes info']);
+    const options = {
+        method: 'GET',
+        url: 'https://sportscore1.p.rapidapi.com/leagues/26',
+        headers: {
+            'x-rapidapi-key': '457a49cbd2msh587809b267ee184p1965ffjsn4b06828b27cb',
+            'x-rapidapi-host': 'sportscore1.p.rapidapi.com'
+        },
+    };
+    const fetchData = useCallback(() => {
+        axios(options)
+
+            .then((response) => {
+                console.log(response)
+                setResponseData(response.data)
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    }, [])
+    return (
+        <div className="container-fluid p-3">
+            <button type='button' onClick={fetchData}> click for football data
+            </button>
+            <table border={2}>
+                <li>
+                    {JSON.stringify(responseData, null, 4)} <br/>
+                </li>
+            </table>
+        </div>
+    );
 }
 
 export default App;
