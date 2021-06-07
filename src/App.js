a132e98929e8ccc0ba3877ae6133bd6700e9590f
AvoKoppas <avo.okas@live.com>
2021-06-07 14:12:30 +0300

    IMDB apiWorks with zero design

--- src/App.js
+++ src/App.js
@@ -4,41 +4,44 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
     const [responseData, setResponseData] = useState(['']);
-    const [endDate, setEndDate] = useState(['']);
-    const [leagueData, setLeagueData] = useState(['']);
+    const [movieTitle, setMovieTitle] = useState(['']);
+    const [yearOfRelease, setYearOfRelease] = useState(['']);
+    const [stars, setStars] = useState(['']);
     const options = {
         method: 'GET',
-        url: 'https://sportscore1.p.rapidapi.com/leagues/26',
+        url: 'https://imdb8.p.rapidapi.com/auto-complete',
+        params: {q: 'homeland'},
         headers: {
             'x-rapidapi-key': '457a49cbd2msh587809b267ee184p1965ffjsn4b06828b27cb',
-            'x-rapidapi-host': 'sportscore1.p.rapidapi.com'
-        },
+            'x-rapidapi-host': 'imdb8.p.rapidapi.com'
+        }
     };
     const fetchData = useCallback(() => {
         axios(options)
 
             .then((response) => {
-                console.log(response.data)
-                setResponseData(response.data.data);
-                setEndDate(response.data.data.end_date);
-                setLeagueData(response.data.data.facts[0]);
+                console.log(response)
+                setResponseData(response.data.d[0]);
+                setMovieTitle(response.data.d[0].l);
+                setYearOfRelease(response.data.d[0].y);
+                setStars(response.data.d[0].s);
             })
             .catch((error) => {
                 console.log(error)
             })
     }, [options]);
-    console.log(responseData);
-    console.log(responseData.end_date);
-    console.log(responseData.facts);
+    console.log(responseData.l);
     return (
-        <div className="container-fluid p-3">
-            <button type='button' onClick={fetchData}> click for football data
+        <div>
+            <input type={<textarea name="title" id="titleId" cols="30" rows="10"/>}/>
+            <button type='button' onClick={fetchData}> click for movie data
             </button>
             <p>
-                The facts: {leagueData && leagueData.facts}
+                The movie: {movieTitle}<br/>
+                The year of release: {yearOfRelease}<br/>
+                The stars: {stars}<br/>
             </p>
-            <p>The end: {endDate}
-            </p>
+
         </div>
     );
 }
